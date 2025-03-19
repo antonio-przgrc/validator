6b3359331d147f3ef6d82c4ce3797d5d7d575561
Antonio Pérez García <antoniopgarcia95@gmail.com>
2025-03-19 12:33:31 +0100

    Añadida conexión sql

--- app.py
+++ app.py
@@ -1,19 +1,9 @@
-import pyodbc
 import cv2
 import pyautogui
 import time
-# Consulta básica a la base de datos (modificar más tarde)
-# def consulta():
-#     try:
-#         with pyodbc.connect(conn_str) as conn:
-#             cursor = conn.cursor()
-#             cursor.execute(consulta)  # Modifica la consulta según necesites
-#             datos = cursor.fetchall()
-#             for linea in datos:
-#                 linea[1] = linea[1].replace(microsecond=0)
-#         return datos
-#     except Exception as e:
-#         return [], []
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
 
 class validator():
     def ubicacion_app(self):
@@ -21,14 +11,14 @@ class validator():
             pyautogui.locateOnScreen(self.skritsga, confidence=0.8)
             try:
                 pyautogui.locateOnScreen(self.salidas, confidence=0.8)
-                self.posicion = 'inicio_salidas'
+                self.estado = 'inicio_salidas'
             except:
                 pos = pyautogui.locateCenterOnScreen(self.menusalidas,confidence=0.8)
                 pyautogui.click(pos)
                 time.sleep(1)
                 try:
                     pyautogui.locateOnScreen(self.salidas, confidence=0.8)
-                    self.posicion = 'inicio_salidas'
+                    self.estado = 'inicio_salidas'
                 except:
                     print('No responde la pantalla')
                     exit
@@ -37,25 +27,29 @@ class validator():
             try:
                 pyautogui.locateOnScreen(self.albarancliente, confidence=0.8)
                 print('Aplicación en menú albaranes')
-                self.posicion = 'menu_albaran'
+                self.estado = 'menu_albaran'
             except:
                 print('No se encuentra aplicación')
                 exit
+        if self.estado == '':
+            exit
 
     def __init__(self):
         # almacén
         self.almacen = '1'
 
         # posición en la aplicacion
-        self.posicion = ''
+        self.estado = ''
 
         # conexión a base de datos
         self.DB_SERVER = '212.81.135.183'
         self.DB_DATABASE = 'BD_SIERRASUR'
         self.DB_USER = 'SierraSur_Consulta'
         self.DB_PASSWORD = 'SierraSur2025@#'
-        self.conn_str = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={self.DB_SERVER};DATABASE={self.DB_DATABASE};UID={self.DB_USER};PWD={self.DB_PASSWORD}'
-        
+        connection_string = f"DRIVER=ODBC Driver 17 for SQL Server;SERVER={self.DB_SERVER};DATABASE={self.DB_DATABASE};UID={self.DB_USER};PWD={self.DB_PASSWORD}"
+        connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
+        engine = create_engine(connection_url)
+
         # imágenes para búsqueda
         self.skritsga = 'pictures/skritsga.png'
         self.albarancliente = 'pictures/albarancliente.png'
@@ -66,6 +60,9 @@ class validator():
         # Comprobar localización en la aplicación
         self.ubicacion_app()
 
-
+    def run(self):
+        # Consulta SQL de albaranes pendientes de validar
+        print('Hola')
 
 app = validator()
+app.run()
